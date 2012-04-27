1eb107f0d266d083265a2ae6148d8c594bf94cd7
Jake Berendes <jacobberendes@gmail.com>
2012-04-27 17:54:34 -0500

    Found to work in Chrome version 19 and up beta,canary,chromium

--- devtools.js
+++ devtools.js
@@ -1,15 +1,16 @@
-
-
-
-// Create the visible DevTools panel/tab.
-function () {
-  if ("devtools" in chrome &&
-      chrome.devtools.panel) {
-    chrome.devtools.panels.create("SassSee",
-        "sasssee-32.png", "devtools.html");
-
-  } else {
-    webInspector.panels.create("SassSee", "sasssee-32.png",
-                               "devtools.html");
-  }
-};
+(function(dt) {
+                var requestUrl;
+    
+                dt.panels.create("SassSee", "sasssee-icon.png", "Panel.html");
+    
+                dt.resources.onNavigated.addListener(function(url) {
+                    requestUrl = url;
+                });
+    
+                dt.resources.onFinished.addListener(function(resource) {
+                    if (resource.request.url === requestUrl) {
+                        // TODO handle request, communicate with panel.html to modify content
+                    }
+                });
+            })(chrome.devtools); 
+            
